6ac6c38f5c630a6780b923b47e9867aa59b64f8f
Maha-India <131330837+Maha-India@users.noreply.github.com>
2024-09-23 12:53:54 +0530

    Update firebase.js

--- src/server/firebase.js
+++ src/server/firebase.js
@@ -1,8 +1,14 @@
 import firebase from "firebase";
 
 var firebaseConfig = {
-  apiKey: "", // Add API Key
-  databaseURL:"" // Add databaseURL
+  apiKey: "AIzaSyC_v4UWFhJlXjrnQo26ZKqgx6n7cxDxNEY",
+  authDomain: "bharat-conferencing.firebaseapp.com",
+  databaseURL: "https://bharat-conferencing-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "bharat-conferencing",
+  storageBucket: "bharat-conferencing.appspot.com",
+  messagingSenderId: "243033188049",
+  appId: "1:243033188049:web:27ac639d6b6687dc0dc7f0",
+  measurementId: "G-5F8MG73JT8"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
